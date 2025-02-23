7ec3aae9c52595c6a990e160ae28fba7ca297a71
groovygnome <jrp810@proton.me>
2025-02-22 19:56:09 -0500

    add calculator.js, index.html, and style.css. implement operate, add, subtract, multiply and divide functions in calculator.js. start buttons on index.html

---
+++ calculator.js
@@ -0,0 +1,26 @@
+let val1;
+let val2;
+let operator;
+
+function operate(num1, op, num2) {
+    if (op === '+') return add(num1, num2);
+    if (op === '-') return subtract(num1, num2);
+    if (op === '*') return multiply(num1, num2);
+    if (op === '/') return divide(num1, num2);
+}
+
+function add(num1, num2) {
+    return Number(num1) + Number(num2);
+}
+
+function subtract(num1, num2) {
+    return Number(num1) - Number(num2);
+}
+
+function multiply(num1, num2) {
+    return Number(num1) * Number(num2);
+}
+
+function divide(num1, num2) {
+    return Number(num1) / Number(num2);
+}
