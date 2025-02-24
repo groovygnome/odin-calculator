39413c029cd8e3a65f9f02991eaabff5a06fd2c4
groovygnome <jrp810@proton.me>
2025-02-24 02:13:46 -0500

    add functionality of calculator buttons, and display

--- calculator.js
+++ calculator.js
@@ -1,6 +1,42 @@
-let val1;
-let val2;
+let val1 = '';
+let val2 = '';
 let operator;
+let answered = false;
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => button.addEventListener("click", () => displayInput(button, button.textContent)));
+
+function displayInput(button, input) {
+    if (button.className === "equals" || (button.className === "operator" && val1 != '' && val2 != '')) {
+        let ans = operate(val1, operator, val2);
+        display.textContent = ans;
+        val1 = ans;
+        val2 = '';
+        operator = undefined;
+        answered = true;
+        if (button.className === "operator") {
+            operator = button.textContent;
+            display.textContent += ' ' + input + ' ';
+        }
+    } else if (button.className === "number") {
+        if (operator) {
+            val2 += input;
+            display.textContent += input;
+        } else if (!answered) {
+            val1 += input;
+            display.textContent += input;
+        }
+    } else if (button.className === "operator" && !operator && val1 != '') {
+        operator = input;
+        display.textContent += ' ' + input + ' ';
+    } else if (button.className === "clear") {
+        display.textContent = '';
+        val1 = '';
+        val2 = '';
+        operator = undefined;
+        answered = false;
+    }
+}
 
 function operate(num1, op, num2) {
     if (op === '+') return add(num1, num2);
